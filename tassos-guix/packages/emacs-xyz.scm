66842303a9ea3c3609ba7631667880c0dc282d2d
Tassos Manganaris <tassos.manganaris@gmail.com>
2021-11-05 12:14:09 -0400

    Almost completed packaging emacs-tree-sitter

--- tassos-guix/packages/emacs-xyz.scm
+++ tassos-guix/packages/emacs-xyz.scm
@@ -1,11 +1,10 @@
 (define-module (tassos-guix packages emacs-xyz)
   #:use-module (gnu packages llvm)
+  #:use-module (gnu packages rust)
   #:use-module (gnu packages crates-io)
   #:use-module (gnu packages crates-graphics)
   #:use-module (gnu packages emacs-xyz)
-
   #:use-module (tassos-guix packages crates-io)
-
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix download)
@@ -14,162 +13,22 @@
   #:use-module (guix build-system cargo)
   #:use-module ((guix licenses) #:prefix license:))
 
-;; (define-public emacs-tsc-dyn
-;;   (package
-;;     (name "emacs-tsc-dyn")
-;;     (version "0.15.2")
-;;     (source
-;;      (origin
-;;        (method url-fetch)
-;;        (uri
-;; 	"file:///home/tassos/software/emacs-tsc-dyn")
-;;        (sha256
-;; 	(base32
-;; 	 "08lmfr7n2yvfmwrr7ifp6pp9c9vd16zkf1wvyl3f13dj849j8qzm"))))
-;;     (build-system cargo-build-system)
-;;     (native-inputs
-;;      `(("clang" ,clang)))
-;;     (arguments
-;;      `(#:cargo-inputs
-;;        (("rust-emacs" ,rust-emacs-0.17)
-;; 	("rust-libloading" ,rust-libloading-0.7)
-;; 	("rust-tree-sitter" ,rust-tree-sitter-0.19)
-;; 	("rust-once-cell" ,rust-once-cell-1))))
-;;     (home-page
-;;      "https://github.com/emacs-tree-sitter/elisp-tree-sitter/tree/master/core")
-;;     (synopsis "Emacs Tree-Sitter Dynamic Module")
-;;     (description
-;;      "This is the core APIs of the Emacs binding for Tree-sitter, an incremental
-;; parsing system.")
-;;     (license #f)))
-
-;; (define-public emacs-vterm
-;;   (let ((version "0.0.1")
-;;         (revision "0")
-;;         (commit "a670b786539d3c8865d8f68fe0c67a2d4afbf1aa"))
-;;     (package
-;;       (name "emacs-vterm")
-;;       (version (git-version version revision commit))
-;;       (source (origin
-;;                 (method git-fetch)
-;;                 (uri (git-reference
-;;                       (url "https://github.com/akermu/emacs-libvterm")
-;;                       (commit commit)))
-;;                 (file-name (git-file-name name version))
-;;                 (sha256
-;;                  (base32
-;;                   "0s244crjkbzl2jhp9m4sm1xdhbpxwph0m3jg18livirgajvdz6hn"))))
-;;       (build-system emacs-build-system)
-;;       (arguments
-;;        `(#:modules ((guix build emacs-build-system)
-;;                     ((guix build cmake-build-system) #:prefix cmake:)
-;;                     (guix build emacs-utils)
-;;                     (guix build utils))
-;; 		   #:imported-modules (,@%emacs-build-system-modules
-;; 				       (guix build cmake-build-system))
-;; 		   #:phases
-;; 		   (modify-phases %standard-phases
-;; 		     (add-after 'unpack 'substitute-vterm-module-path
-;; 		       (lambda* (#:key outputs #:allow-other-keys)
-;; 			 (chmod "vterm.el" #o644)
-;; 			 (emacs-substitute-sexps "vterm.el"
-;; 			   ("(require 'vterm-module nil t)"
-;; 			    `(module-load
-;; 			      ,(string-append (assoc-ref outputs "out")
-;; 					      "/lib/vterm-module.so"))))
-;; 			 #t))
-;; 		     (add-after 'build 'configure
-;; 		       ;; Run cmake.
-;; 		       (lambda* (#:key outputs #:allow-other-keys)
-;; 			 ((assoc-ref cmake:%standard-phases 'configure)
-;; 			  #:outputs outputs
-;; 			  #:out-of-source? #f
-;; 			  #:configure-flags '("-DUSE_SYSTEM_LIBVTERM=ON"))
-;; 			 #t))
-;; 		     (add-after 'configure 'make
-;; 		       ;; Run make.
-;; 		       (lambda* (#:key (make-flags '()) outputs #:allow-other-keys)
-;; 			 ;; Compile the shared object file.
-;; 			 (apply invoke "make" "all" make-flags)
-;; 			 ;; Move the file into /lib.
-;; 			 (install-file
-;; 			  "vterm-module.so"
-;; 			  (string-append (assoc-ref outputs "out") "/lib"))
-;; 			 #t)))
-;; 		   #:tests? #f))
-;;       (native-inputs
-;;        `(("cmake" ,cmake-minimal)
-;;          ("libtool" ,libtool)
-;;          ("libvterm" ,libvterm)))
-;;       (home-page "https://github.com/akermu/emacs-libvterm")
-;;       (synopsis "Emacs libvterm integration")
-;;       (description "This package implements a bridge to @code{libvterm} to
-;; display a terminal in an Emacs buffer.")
-;;       (license license:gpl3+))))
-
-;; (define-public emacs-tsc
-;;   (package
-;;    (name "emacs-tsc")
-;;    (version "20210912.1211")
-;;    (source
-;;     (origin
-;;      (method url-fetch)
-;;      (uri
-;;       "file:///home/tassos/software/emacs-tsc")
-;;      (sha256
-;;       (base32
-;;        "08lmfr7n2yvfmwrr7ifp6pp9c9vd16zkf1wvyl3f13dj849j8qzm"))))
-;;    (build-system emacs-build-system)
-;;    (home-page
-;;     "https://github.com/emacs-tree-sitter/elisp-tree-sitter")
-;;    (synopsis "Core Tree-sitter APIs")
-;;    (description
-;;     "This is the core APIs of the Emacs binding for Tree-sitter, an incremental
-;; parsing system.")
-;;    (license #f)))
-
-;; (define-public emacs-tree-sitter
-;;   (package
-;;    (name "emacs-tree-sitter")
-;;    (version "20210912.1211")
-;;    (source
-;;     (origin
-;;      (method git-fetch)
-;;      (uri
-;;       (git-reference
-;;        (url "file:///home/tassos/software/elisp-tree-sitter")
-;;        (commit "d694fbeaa5a0dcc9be8fc61603a4ddae78dec65d")))
-;;      (sha256
-;;       (base32
-;;        "082797y5h0n4kdyxy32gxwmc7s5s2cqrrlsp1q03hqrgvvm3xasz"))))
-;;    (build-system emacs-build-system)
-;;    (propagated-inputs `(("emacs-tsc" ,emacs-tsc)))
-;;    (home-page
-;;     "https://github.com/emacs-tree-sitter/elisp-tree-sitter")
-;;    (synopsis "Incremental parsing system")
-;;    (description
-;;     "This is the base framework of the Emacs binding for Tree-sitter, an
-;; incremental parsing system. It includes a minor mode that provides a
-;; buffer-local syntax tree that is updated on every text change. This minor
-;; mode is the base for other libraries to build on. An example is the included
-;; code-highlighting minor mode.")
-;;    (license #f)))
-
 (define-public emacs-popper
   (package
-   (name "emacs-popper")
-   (version "20211011.113")
-   (source
-    (origin
-     (method url-fetch)
-     (uri (string-append "https://melpa.org/packages/popper-" version ".tar"))
-     (sha256
-      (base32 "05i3fqdacdg41h0h3j7lvndljjiya2r8g1dkcxs00fq5xy5rcklq"))))
-   (build-system emacs-build-system)
-   (home-page "https://github.com/karthink/popper")
-   (synopsis "Summon and dismiss buffers as popups")
-   (description
-    "Popper is a minor-mode to tame the flood of ephemeral windows Emacs
+    (name "emacs-popper")
+    (version "20211011.113")
+    (source
+     (origin
+       (method url-fetch)
+       (uri
+        (string-append "https://melpa.org/packages/popper-" version ".tar"))
+       (sha256
+        (base32 "05i3fqdacdg41h0h3j7lvndljjiya2r8g1dkcxs00fq5xy5rcklq"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/karthink/popper")
+    (synopsis "Summon and dismiss buffers as popups")
+    (description
+     "Popper is a minor-mode to tame the flood of ephemeral windows Emacs
 produces, while still keeping them within arm's reach. Designate any
 buffer to \"popup\" status, and it will stay out of your way. Disimss
 or summon it easily with one key. Cycle through all your \"popups\" or
@@ -214,29 +73,30 @@ There are other customization options, such as the ability to suppress
 certain popups and keep them from showing. Please customize the popper group
 for details.
 ")
-   (license #f)))
+    (license #f)))
 
 (define-public emacs-consult-dir
   (package
-   (name "emacs-consult-dir")
-   (version "20211007.2352")
-   (source
-    (origin
-     (method url-fetch)
-     (uri (string-append
-           "https://melpa.org/packages/consult-dir-"
-           version
-           ".el"))
-     (sha256
-      (base32 "1gszscjk4gp7qadqyz8vi584h9kz59rk69lhi0zrnvszs8znzgpa"))))
-   (build-system emacs-build-system)
-   (propagated-inputs
-    `(("emacs-consult" ,emacs-consult)
-      ("emacs-project" ,emacs-project)))
-   (home-page "https://github.com/karthink/consult-dir")
-   (synopsis "Insert paths into the minibuffer prompt")
-   (description
-    "Consult-dir implements commands to easily switch between \"active\"
+    (name "emacs-consult-dir")
+    (version "20211007.2352")
+    (source
+     (origin
+       (method url-fetch)
+       (uri
+        (string-append
+         "https://melpa.org/packages/consult-dir-"
+         version
+         ".el"))
+       (sha256
+        (base32 "1gszscjk4gp7qadqyz8vi584h9kz59rk69lhi0zrnvszs8znzgpa"))))
+    (build-system emacs-build-system)
+    (propagated-inputs
+     `(("emacs-consult" ,emacs-consult)
+       ("emacs-project" ,emacs-project)))
+    (home-page "https://github.com/karthink/consult-dir")
+    (synopsis "Insert paths into the minibuffer prompt")
+    (description
+     "Consult-dir implements commands to easily switch between \"active\"
 directories. The directory candidates are collected from user bookmarks,
 projectile project roots (if available), project.el project roots and recentf
 file locations. The `default-directory' variable not changed in the process.
@@ -276,4 +136,109 @@ required to install this package.
 To change directory sources or their ordering, customize
 `consult-dir-sources'.
 ")
-   (license #f)))
+    (license #f)))
+
+(define-public emacs-tsc-module
+  (package
+    (name "emacs-tsc-module")
+    (version "20210912.1211")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://melpa.org/packages/tsc-" version ".tar"))
+       (sha256
+        (base32 "1wrixz8ymh5i1kk5ryq89f789r51nlfs1bclbp2a2jy4ljp5lcr7"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack-rust-crates 'remove-patch-section
+           (lambda _
+             ;; Remove the patch section from Cargo.toml
+             (substitute* "Cargo.toml"
+               (("\\[patch.crates-io.tree-sitter]") "")
+               (("git = \"https://github.com/tree-sitter/tree-sitter\"") ""))
+             #t))
+         (replace 'install
+           (lambda* (#:key outputs #:allow-other-keys)
+             ;; Copy the dynamic module result to the output.
+             (install-file
+              "target/release/libtsc_dyn.so"
+              (assoc-ref outputs "out"))
+             #t)))
+       #:cargo-inputs
+       (("rust-emacs" ,rust-emacs-0.17)
+        ("rust-libloading" ,rust-libloading-0.7)
+        ("rust-tree-sitter" ,rust-tree-sitter-0.19)
+        ("rust-once-cell" ,rust-once-cell-1))))
+    (native-inputs
+     `(("clang" ,clang-12)))
+    (home-page "https://github.com/emacs-tree-sitter/elisp-tree-sitter")
+    (synopsis "Core Tree-sitter APIs")
+    (description
+     "This is the core APIs of the Emacs binding for Tree-sitter, an incremental
+parsing system.
+")
+    (license #f)))
+
+(define-public emacs-tsc
+  (package
+    (name "emacs-tsc")
+    (version "20210912.1211")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://melpa.org/packages/tsc-" version ".tar"))
+       (sha256
+        (base32 "1wrixz8ymh5i1kk5ryq89f789r51nlfs1bclbp2a2jy4ljp5lcr7"))))
+    (build-system emacs-build-system)
+    (arguments
+     `(#:include
+       '("^[^/]*\\.el$" "^tsc-dyn.so$" "^DYN-VERSION$" "^[^/]*\\.info$" "^doc/.*\\.info$")
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'add-local-dynamic-module
+           (lambda* (#:key inputs #:allow-other-keys)
+             (copy-recursively
+              (assoc-ref inputs "emacs-tsc-module") ".")
+             (rename-file "./libtsc_dyn.so" "./tsc-dyn.so")
+             (let ((port (open-output-file "DYN-VERSION")))
+               (display "LOCAL" port)
+               (close-port port))
+             #t)))))
+    (native-inputs
+     `(("emacs-tsc-module" ,emacs-tsc-module)))
+    (home-page "https://github.com/emacs-tree-sitter/elisp-tree-sitter")
+    (synopsis "Core Tree-sitter APIs")
+    (description
+     "This is the core APIs of the Emacs binding for Tree-sitter, an incremental
+parsing system.
+")
+    (license #f)))
+
+(define-public emacs-tree-sitter
+  (package
+    (name "emacs-tree-sitter")
+    (version "20210912.1211")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://melpa.org/packages/tree-sitter-"
+             version
+             ".tar"))
+       (sha256
+        (base32 "1x20gl81x2pr6hm84pn9s89jmn78dj5pvy36bhw69cka7nmc0lfg"))))
+    (build-system emacs-build-system)
+    (propagated-inputs
+     `(("emacs-tsc" ,emacs-tsc)))
+    (home-page "https://github.com/emacs-tree-sitter/elisp-tree-sitter")
+    (synopsis "Incremental parsing system")
+    (description
+     "This is the base framework of the Emacs binding for Tree-sitter, an
+incremental parsing system. It includes a minor mode that provides a
+buffer-local syntax tree that is updated on every text change. This minor
+mode is the base for other libraries to build on. An example is the included
+code-highlighting minor mode.
+")
+    (license #f)))
