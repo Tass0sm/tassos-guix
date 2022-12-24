4d6d3a0b10475c64e9d1ed8771cef41dd306c211
Tassos Manganaris <tassos.manganaris@gmail.com>
2022-12-23 20:20:15 -0500

    Added package for cache-env

--- tassos-guix/packages/shellutils.scm
+++ tassos-guix/packages/shellutils.scm
@@ -1,10 +1,31 @@
 (define-module (tassos-guix packages shellutils)
+  #:use-module (gnu packages crates-io)
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
+  #:use-module (guix build-system cargo)
   #:use-module (guix build-system copy)
   #:use-module ((guix licenses) #:prefix license:))
+
+(define-public cache-env
+  (package
+    (name "cache-env")
+    (version "0.1.0")
+    (source
+     (local-file "/home/tassosm/software/cache-env"
+                 #:recursive? #t
+                 #:select? (git-predicate "/home/tassosm/software/cache-env")))
+    (build-system cargo-build-system)
+    (arguments
+     (list #:cargo-inputs
+           `(("rust-clap" ,rust-clap-3))))
+    (home-page "https://github.com/sindresorhus/pure")
+    (synopsis "Save and retrieve environment states.")
+    (description
+     "Save and retrieve environment states. Useful for unloading guix
+ profiles.")
+    (license license:expat)))
 
 (define-public zsh-pure
   (package
